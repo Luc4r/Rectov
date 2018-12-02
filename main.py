c07697c57075410360de9a0ec2e5109692e787d1
Karol Kapłanek <fhlucar@gmail.com>
2018-12-02 13:37:00 +0100

    modified main file to correctly render new game objects (player, walls)

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 
 from src.components.Menu import Menu
+from src.components.Wall import Wall
+from src.components.Player import Player
 
 from src.utils.getDataFromJSON import getDataFromJSON
 
@@ -15,6 +17,36 @@ def main():
 
   while not menu.isGameRunning:
     menu.update()
+
+  walls = []
+  for x in range(32):
+    Wall(screen, colors, walls, x, y=17)
+
+  player = Player(screen, colors)
+
+  clock = pygame.time.Clock()
+  running = True
+  while running:
+  	# Max fps
+    clock.tick(60)
+    # Special keys events
+    events = pygame.event.get()
+    for event in events:
+      if event.type == pygame.QUIT:
+        running = False
+      if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+        running = False
+    # Update player if key pressed
+    player.update()
+    # Draw background
+    screen.fill((22, 22, 22))
+    # Draw player
+    player.displayPlayer()
+    # Draw walls
+    for wall in walls:
+      wall.displayWall()
+    # Update screen
+    pygame.display.update()
 
 if __name__ == '__main__':
   main()
