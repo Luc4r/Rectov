b1431e94e4a58d31851367f7ceee19db4d492a1d
Karol Kapłanek <fhlucar@gmail.com>
2018-12-08 12:03:00 +0100

    LoadingScreen - fixed displaying player score

--- src/components/LoadingScreen.py
+++ src/components/LoadingScreen.py
@@ -3,11 +3,11 @@ import pygame
 from src.utils.displayText import displayTextCentered
 
 class LoadingScreen:
-  def __init__(self, screen, colors, levelName, currentScore):
+  def __init__(self, screen, colors, levelName, playerScore):
     self.screen = screen
     self.colors = colors
     self.levelName = levelName
-    self.currentScore = currentScore
+    self.playerScore = playerScore
 
   def drawLevelName(self):
     displayTextCentered(self.screen, text=self.levelName, y=200, fontSize=96)
@@ -15,7 +15,7 @@ class LoadingScreen:
   def drawPlayerScore(self):
     coinObject = pygame.Rect(550, 665, 10, 10)
     pygame.draw.rect(self.screen, self.colors["yellow"], coinObject)
-    displayTextCentered(self.screen, text=self.currentScore, y=660)
+    displayTextCentered(self.screen, text="{:06d}".format(self.playerScore[0]), y=660)
 
   def drawLoadingScreen(self):
     self.screen.fill((22, 22, 22))
