8b2f1bd2b93ba3f5ddb64806037ff7ae7fb527ff
Karol Kapłanek <fhlucar@gmail.com>
2018-12-21 13:27:00 +0100

    TransitionSurface - added optional background render and skip option

--- src/components/TransitionSurface.py
+++ src/components/TransitionSurface.py
@@ -1,32 +1,49 @@
 import pygame
 
 class TransitionSurface:
-  def __init__(self, screen, fillColor):
+  def __init__(self, screen, clock, fillColor):
     self.screen = screen
+    self.clock = clock
     self.transitionScreen = pygame.Surface((1280, 720))
     self.transitionScreen.fill(fillColor)
 
-  def fadeIn(self, backgroundFunction):
+  def fadeIn(self, backgroundFunction = None):
     alpha = 255
     self.transitionScreen.set_alpha(alpha)
     while alpha > 0:
+      # Max fps
+      self.clock.tick(60)
+      # Skip animation when any key is pressed or quit button pressed
+      events = pygame.event.get()
+      for event in events:
+        if event.type == pygame.KEYDOWN or event.type == pygame.QUIT:
+          return
       # Draw background
-      backgroundFunction()
+      if backgroundFunction is not None:
+        backgroundFunction()
       # Change alpha of the transition surface
-      alpha -= 2
+      alpha -= 4
       self.transitionScreen.set_alpha(alpha)
       # Draw updated transition surface on screen
       self.screen.blit(self.transitionScreen, (0, 0))
       pygame.display.update()
 
-  def fadeOut(self, backgroundFunction):
+  def fadeOut(self, backgroundFunction = None):
     alpha = 0
     self.transitionScreen.set_alpha(alpha)
-    while alpha < 255:
+    while alpha < (255 if backgroundFunction is not None else 80):
+      # Max fps
+      self.clock.tick(60)
+      # Skip animation when any key is pressed or quit button pressed
+      events = pygame.event.get()
+      for event in events:
+        if event.type == pygame.KEYDOWN or event.type == pygame.QUIT:
+          return
       # Draw background
-      backgroundFunction()
+      if backgroundFunction is not None:
+        backgroundFunction()
       # Change alpha of the transition surface
-      alpha += 2
+      alpha += (4 if backgroundFunction is not None else 1)
       self.transitionScreen.set_alpha(alpha)
       # Draw updated transition surface on screen
       self.screen.blit(self.transitionScreen, (0, 0))
