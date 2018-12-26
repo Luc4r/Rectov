243671328234171d15caed2a213c7faee240ed21
Karol Kapłanek <fhlucar@gmail.com>
2018-12-26 21:56:00 +0100

    new game component - PauseScreen

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import pygame
 from src.components.Menu import Menu
 from src.components.TransitionSurface import TransitionSurface
 from src.components.LoadingScreen import LoadingScreen
+from src.components.DeathScreen import DeathScreen
+from src.components.PauseScreen import PauseScreen
 from src.components.UserInterface import UserInterface
 from src.components.Player import Player
 from src.components.Enemy import Enemy
@@ -12,6 +14,7 @@ from src.components.Level import Level
 from src.utils.getDataFromJSON import getDataFromJSON
 
 colors = getDataFromJSON("src/config/colors.json")
+clock = pygame.time.Clock()
 
 def main():
   pygame.init()
@@ -19,24 +22,30 @@ def main():
   screen = pygame.display.set_mode((1280, 720))
   screen.fill(colors["background"])
 
-  transition = TransitionSurface(screen, colors["background"])
+  transition = TransitionSurface(screen, clock, colors["background"])
   menu = Menu(screen, colors)
 
   transition.fadeIn(backgroundFunction=menu.drawMenu)
 
   while not menu.isGameRunning:
     menu.drawMenu()
+    pygame.display.update()
 
   transition.fadeOut(backgroundFunction=menu.drawMenu)
 
   walls = []
   platforms = []
   coins = []
-  playerScore = [0] # Mutable object (list) - used for easy update every object using it
+  isGameRunning = [True]
+  isGamePaused = [False]
+  playerScore = [0]
+
   level = Level(screen, walls, platforms, coins, colors, levelName="lvl-1")
-  player = Player(screen, walls, platforms, coins, colors, playerScore, main)
+  player = Player(screen, walls, platforms, coins, colors, playerScore)
   enemy = Enemy(screen, walls, platforms, player, colors, isGameRunning)
   loadingScreen = LoadingScreen(screen, colors, "1 - Test Area", playerScore)
+  deathScreen = DeathScreen(screen, colors, playerScore, confirmTransition=transition.fadeOut, backToMainMenu=main)
+  pauseScreen = PauseScreen(screen, colors, isGamePaused, confirmTransition=transition.fadeOut, backToMainMenu=main)
   ui = UserInterface(screen, colors, "1 - Test Area", playerScore)
 
   level.buildLevel()
@@ -53,19 +62,48 @@ def main():
   transition.fadeOut(backgroundFunction=loadingScreen.drawLoadingScreen)
   transition.fadeIn(backgroundFunction=level.drawLevel)
 
-  clock = pygame.time.Clock()
-  running = True
-  while running:
+  while isGameRunning[0]:
     # Max fps
     clock.tick(60)
-    # Update player
+    # Check special events
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        transition.fadeOut()
+        main()
+      if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_p:
+          isGamePaused[0] = True
+          while isGamePaused[0]:
+            # Max fps
+            clock.tick(60)
+            # Draw pause screen elements
+            screen.fill(colors["background"])
+            enemy.drawEnemy()
+            level.drawLevel()
+            player.drawPlayer()
+            pauseScreen.drawPauseScreen()
+            # Update screen
+            pygame.display.update()
+    # Update moveable objects
     player.update()
+    enemy.update()
     # Draw game elements
     screen.fill(colors["background"])
     enemy.drawEnemy()
     level.drawLevel()
     player.drawPlayer()
     ui.drawUI()
+    # Update screen
+    pygame.display.update()
+
+  transition.fadeOut()
+  transition.fadeIn(backgroundFunction=deathScreen.drawDeathScreen)
+
+  while True: # only way to get there is to die or finish the game - otherwise it just gets back to main
+    # Max fps
+    clock.tick(60)
+    # Draw end game screen
+    deathScreen.drawDeathScreen()
     # Update screen
     pygame.display.update()
 
---
+++ src/components/PauseScreen.py
@@ -0,0 +1,71 @@
+import pygame
+
+from src.utils.drawText import drawText, drawTextCentered
+
+class PauseScreen:
+  def __init__(self, screen, colors, isPaused, confirmTransition, backToMainMenu):
+    self.screen = screen
+    self.colors = colors
+    self.isPaused = isPaused
+    self.confirmTransition = confirmTransition
+    self.backToMainMenu = backToMainMenu
+
+    self.activeOption = 0
+    self.pauseSurface = pygame.Surface((1280, 720), pygame.SRCALPHA)
+
+  def drawBackground(self):
+    self.pauseSurface.fill((0, 0, 0, 200))
+
+  def drawPauseMessage(self):
+    drawTextCentered(self.pauseSurface, text="PAUSED", y=200, fontSize=96)
+
+  def drawOptionDots(self):
+    firstDot = pygame.Rect(500, 383, 20, 20)
+    secondDot = pygame.Rect(500, 423, 20, 20)
+    if self.activeOption == 0:
+      pygame.draw.rect(self.pauseSurface, self.colors["white"], firstDot)
+      pygame.draw.rect(self.pauseSurface, self.colors["grey"], secondDot)
+    elif self.activeOption == 1:
+      pygame.draw.rect(self.pauseSurface, self.colors["grey"], firstDot)
+      pygame.draw.rect(self.pauseSurface, self.colors["white"], secondDot)
+
+  def drawOptions(self):
+    self.drawOptionDots()
+    inactiveColor = self.colors["grey"]
+    activeColor = self.colors["white"]
+
+    if self.activeOption == 0:
+      drawText(self.pauseSurface, text="RESUME", x=560, y=380, color=activeColor, fontSize=36)
+      drawText(self.pauseSurface, text="EXIT", x=560, y=420, color=inactiveColor, fontSize=36)
+    elif self.activeOption == 1:
+      drawText(self.pauseSurface, text="RESUME", x=560, y=380, color=inactiveColor, fontSize=36)
+      drawText(self.pauseSurface, text="EXIT", x=560, y=420, color=activeColor, fontSize=36)  
+
+  def checkInput(self):
+    events = pygame.event.get()
+    for event in events:
+      if event.type == pygame.QUIT:
+        self.confirmTransition()
+        self.backToMainMenu()
+      if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_ESCAPE or event.key == pygame.K_p:
+          self.isPaused[0] = False
+        elif event.key == pygame.K_UP and self.activeOption > 0:
+          self.activeOption -= 1
+        elif event.key == pygame.K_DOWN and self.activeOption < 1:
+          self.activeOption += 1
+        elif event.key == pygame.K_RETURN:
+          if self.activeOption == 0:
+            self.isPaused[0] = False
+          elif self.activeOption == 1:
+            self.confirmTransition()
+            self.backToMainMenu()
+
+  def drawPauseScreen(self):
+    self.checkInput()
+    self.drawBackground()
+    self.drawPauseMessage()
+    self.drawOptions()
+    # Draw pause screen on the screen
+    self.screen.blit(self.pauseSurface, (0, 0))
+    
